d6cbe21fbe1298bb5d24b47b38dcc603df8ad121
Caio Campos <caio.campos1204@gmail.com>
2021-08-05 18:06:37 -0300

    Criada classe dedicada a emitir sons com tons aleatórios e reorganizado

---
+++ src/RandomAudioStreamPlayer.gd
@@ -0,0 +1,17 @@
+extends AudioStreamPlayer
+class_name RandomAudioStreamPlayer
+
+export var pitch_min := 0.85
+export var pitch_max := 1.25
+
+onready var next_random_pitch := get_random_pitch()
+
+func random_pitch_play():
+	pitch_scale = next_random_pitch
+	play()
+	yield(self, "finished")
+	pitch_scale = 1
+	next_random_pitch =  get_random_pitch()
+
+func get_random_pitch() -> float:
+	return rand_range(pitch_min, pitch_max)
