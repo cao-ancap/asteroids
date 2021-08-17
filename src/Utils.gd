1854e8524a86e8ade8a4376908160292c1aa8704
Caio Campos <caio.campos1204@gmail.com>
2021-08-17 18:55:14 -0300

    Adicionadas funções para montar json "salgado"

--- src/Utils.gd
+++ src/Utils.gd
@@ -28,3 +28,23 @@ func rand_bool() -> bool:
 
 func calc_difficulty_to_hp(difficulty: int):
 	return 10 - difficulty
+
+
+func create_signed_json(dict: Dictionary) -> String:
+	dict["_n"] = randi()
+	dict["_h"] = salt_hash(dict)
+	return JSON.print(dict)
+
+
+func salt_hash(dict: Dictionary) -> String:
+	var o := dict.duplicate()
+	o["_n"] = calc_salt(o["_n"] if o["_n"] and o["_n"] != 0 else 249)
+	o["_ns"] = calc_salt(4651)
+	o["_ts"] = "dfjksduhajsnduygzyxndcowicxzujnqwbfuygysc"
+	return JSON.print(o).sha256_text()
+
+
+func calc_salt(n: float) -> String:
+	var x := sin(n) * 10000
+	var ac := acos(n) if n > -1 and n < 1 else acos(1 / n)
+	return "%0.10f" % (x + ac)
